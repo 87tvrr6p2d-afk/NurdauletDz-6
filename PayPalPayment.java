decf6b320f80224cea8171bbbcb6ec01bc75865c
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-02-27 17:53:18 +0500

    Create PayPalPayment.java

---
+++ PayPalPayment.java
@@ -0,0 +1,13 @@
+public class PayPalPayment implements PaymentStrategy {
+
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Оплата " + amount + " ₸ через PayPal: " + email);
+    }
+}
