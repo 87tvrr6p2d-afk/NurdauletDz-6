e9f31c6bd15f38aaa965f570a7bdbabae0326085
87tvrr6p2d-afk <87tvrr6p2d@privaterelay.appleid.com>
2026-02-27 17:54:28 +0500

    Create CryptoPayment.java

---
+++ CryptoPayment.java
@@ -0,0 +1,13 @@
+public class CryptoPayment implements PaymentStrategy {
+
+    private String walletAddress;
+
+    public CryptoPayment(String walletAddress) {
+        this.walletAddress = walletAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Оплата " + amount + " ₸ криптовалютой. Кошелек: " + walletAddress);
+    }
+}
